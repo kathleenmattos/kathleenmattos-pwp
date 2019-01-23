28d3b62bb710921dcee48f3577568bbaa2152d68
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-01-23 15:59:40 -0700

    added persona

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>KM PWP</title>
+	</head>
+	<body>
+		<h1>Kathleen Mattos PWP</h1>
+		<h2>Milestone 1</h2>
+		<h4>Purpose</h4>
+		<p>I would like use this page as a way for potential employers to get to know me and see what I can do.</p>
+		<h4>Audience</h4>
+		<p>The potential audience for this page will probably be mostly potential employers, but will also include professional peers and other developers.</p>
+		<h4>Goal</h4>
+		<p>The goal for my PWP would be to aquire full or part-time work.</p>
+		<h3>Persona</h3>
+		<ul>
+			<li><strong>Name</strong>: John Murphy</li>
+			<li><strong>Gender</strong>: Male</li>
+			<li><strong>Age</strong>: 42</li>
+			<li><strong>Job</strong>: Entreprenuer, owns/operates a startup company that is quickly growing.</li>
+			<li><strong>Technology</strong>:
+				<ul>
+					<li>Laptop:Dell Inspiron 17.3" Intel Core i7 8GB Memory 2TB hard Drive -Silver</li>
+					<li>Phone: Samsung Galaxy S9+</li>
+				</ul>
+			</li>
+			<li><strong>Attitude</strong>:
+			<ul>
+				<li>Always busy, doesn't have time to waste.</li>
+				<li>Highly motivated and a bit of a workoholic. </li>
+			</ul>
+			</li>
+			<li><strong>Needs/Goals</strong>:
+			<ul>
+				<li>Would like to continue to expand his business and needs a better website.</li>
+				<li>Looking to hire an employee to build his website.</li>
+				<li>Needs to quickly be able to see examples of the potential employee's work quickly because he doesn't have a lot of time to be browsing resumes.</li>
+			</ul>
+			</li>
+			<li><strong>Frustrations</strong>:
+			<ul>
+				<li>Doesn't have a lot of time to look at resumes.</li>
+				<li>Old website does not match the vision for the company.</li>
+			</ul>
+			</li>
+			<li><strong></strong>:</li>
+		</ul>
+
+	</body>
+</html>
