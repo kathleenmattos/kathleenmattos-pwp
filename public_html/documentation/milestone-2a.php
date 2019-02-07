656640dd1cdfea5a8147534652817e3c2576092e
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-02-07 12:45:21 -0700

    added name and photo description

--- public_html/documentation/milestone-2a.php
+++ public_html/documentation/milestone-2a.php
@@ -7,10 +7,11 @@
 	<body>
 		<h1>Milestone 2a</h1>
 		<h3>Content Strategy</h3>
+			<p><Strong>Name and Photo:</Strong>At the top of the page there will be a picture and my name.</p>
  			<p><strong>Toolbar:</strong> The toolbar near the top of the page has four buttons; about me, skills, portfolio,contact me. When on eof these is clicked it will automatically scroll down to that section of the page. </p>
 			<p><strong>About Me:</strong> The about me section will contain some info about me that employers may be interested in.</p>
 			<p><strong>Skills:</strong> The skills section will display a list of skills that I have including languages, software/platforms, and other technological skills that aren't as quantifiable.</p>
-			<p><strong>Portfolio:</strong>The portfolio section will contain some example of projects that I have created or contributed to.</p>
+			<p><strong>Portfolio:</strong>The portfolio section will contain some example of projects that I have created or contributed to. It will show a small picture and include a little blurb about the project.</p>
 			<p><strong>Contact Me:</strong> The contact me section will contain text input boxes that when filled out will send an email to the site owner.</p>
 			<p><strong>Find Me:</strong> This section will include links to accounts where they can learn more about me, including github and linkedin.</p>
 		<div><img src="onlineMockup.png" alt="Web Mockup"/></div>
