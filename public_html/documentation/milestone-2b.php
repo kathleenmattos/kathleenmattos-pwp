d155a3604c2e66879a58e6f5a4211a5b68bfc8da
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-02-25 14:06:22 -0700

    made new navbar

--- public_html/documentation/milestone-2b.php
+++ public_html/documentation/milestone-2b.php
@@ -22,7 +22,7 @@
 	<body>
 		<header>
 		<!------------------Navbar------------------------>
-				<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+			<!--	<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
 					<div class="container">
 						<a class="navbar-brand" href="#">Kathleen Mattos</a>
 						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedConentent-7" aria-controls="navBarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
@@ -31,7 +31,7 @@
 						<div class="collapse navbar-collapse" id="navbarSupportedContent-7">
 							<ul class="navbar-nav ml-auto">
 								<li class="nav-item">
-									<a class="nav-link" href="#">About Me </a>
+									<a class="nav-link" href="#">About Me <span class="sr-only">(current)</span> </a>
 								</li>
 								<li class="nav-item">
 									<a class="nav-link" href="#">Skills</a>
@@ -45,7 +45,34 @@
 							</ul>
 						</div>
 					</div>
-				</nav>
+				</nav> -->
+
+
+			<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar px-4 shadow-sm p-3 mb-5 bg-white">
+				<a class="navbar-brand text-white" href="#">Kathleen Mattos</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link text-white" href="#">About Me <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-white" href="#">Skills</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-white" href="#">Portfolio</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-white" href="#">Contact Me</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+
+
 			<!-------------------Full page background image------------------------->
 			<div class="view" id="backgroundImage">
 				<!--Mask and flexbox-->
