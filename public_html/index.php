df013d5b5bda79493af930d87d068066f48dd845
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-03-03 12:08:58 -0700

    added glitch animation

--- public_html/index.php
+++ public_html/index.php
@@ -8,7 +8,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
 		<!--google fonts-->
-		<link href="https://fonts.googleapis.com/css?family=Lato|Nunito" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Lato|Nunito|Roboto:400,700" rel="stylesheet">
 
 		<!--bootstrap css-->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -71,12 +71,19 @@
 				<!--Mask and flexbox-->
 				<div class="mask rgba-black-light align-items-center">
 					<!--content-->
-					<div class="container">
+					<div class="container-fluid">
+
+						<div class="glitchContainer">
+							<h1 class="glitch">Kathleen Mattos</h1>
+						</div>
+
+
 						<!--grid row-->
 						<div class="row">
 							<!--column-->
 							<div class="col-md-12 mb-4 white-text text-right">
 								<!--todo insert blurb-->
+
 							</div>
 						</div>
 					</div>
